f1607639c25182b07bae086f566184ec986d1fff
eric-klimowich <eklimowich@hotmail.com>
2019-08-09 09:34:40 -0400

    Parsed data and narrowed down weather data to current data

--- app.js
+++ app.js
@@ -3,5 +3,6 @@ const request = require('request')
 const url = 'https://api.darksky.net/forecast/d1e63bda71af2e89cefa84b08bd093a1/37.8267,-122.4233'
 
 request({ url: url}, (error, response) => {
-    console.log(response)
+    const data = JSON.parse(response.body)
+    console.log(data.currently)
 })
