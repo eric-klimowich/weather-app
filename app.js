79005c467c64783931d38b55077a542b518db3e3
eric-klimowich <eklimowich@hotmail.com>
2019-08-09 13:48:59 -0400

    Request made to mapbox to obtain longitude and latitude coordinates from a specific location, displayed long/lat

--- app.js
+++ app.js
@@ -8,3 +8,10 @@ request({ url: url, json: true }, (error, response) => {
 
     // it is currently 58 degrees out. there is a 0% chance of rain.
 })
+
+const geoUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiZWtsaW1vd2ljaCIsImEiOiJjano0ZHh1ZngwMTB0M2hyc2FsZTlxazhrIn0.83V5sgB0dgNrqZFqJxUmuw'
+
+request({ url: geoUrl, json: true }, (error, response) => {
+    console.log('Longitude: ' + response.body.features[0].center[0])
+    console.log('Latitude: ' + response.body.features[0].center[1])
+})
