9f106bb4b316eb91df7e00a8c82d6dba21c0f016
eric-klimowich <eklimowich@hotmail.com>
2019-08-09 13:20:28 -0400

    Customized data to display temperature and precipitation percentage, using interpolation

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const request = require('request')
 
 const url = 'https://api.darksky.net/forecast/d1e63bda71af2e89cefa84b08bd093a1/37.8267,-122.4233'
 
-request({ url: url}, (error, response) => {
-    const data = JSON.parse(response.body)
-    console.log(data.currently)
+request({ url: url, json: true }, (error, response) => {
+    // console.log(response.body.currently)
+    console.log(`It is currently ${response.body.currently.temperature} degrees out. There is a ${response.body.currently.precipProbability}% chance of rain.`)
+
+    // it is currently 58 degrees out. there is a 0% chance of rain.
 })
