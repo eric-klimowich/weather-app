c2ef83d2dac6bd769a6dced948975e86a63266cf
eric-klimowich <eklimowich@hotmail.com>
2019-08-09 16:04:35 -0400

    Added error handling for no location provided

--- app.js
+++ app.js
@@ -5,16 +5,21 @@ const forecast = require('./utils/forecast')
 const location = process.argv[2]
 
 geocode(location, (error, locationData) => {
-    if (error) {
-        return console.log(error)
-    }
-    
-    forecast(locationData.longitude, locationData.latitude, (error, forecastData) => {
+    if (location) {
         if (error) {
-            console.log(error)
+            return console.log(error)
         }
         
-        console.log(locationData.location)
-        console.log(forecastData)
-      })
+        forecast(locationData.longitude, locationData.latitude, (error, forecastData) => {
+            if (error) {
+                console.log(error)
+            }
+            
+            console.log(locationData.location)
+            console.log(forecastData)
+          })
+    } else {
+        console.log('Please enter a location!')
+    }
+    
 })
--- utils/forecast.js
+++ utils/forecast.js
@@ -16,9 +16,3 @@ const forecast = (longitude, latitude, callback) => {
 }
 
 module.exports = forecast
-
-// forecast(-75.7088, 44.1545, (error, data) => {
-//     console.log('Error', error)
-//     console.log('Data', data)
-//   })
-
