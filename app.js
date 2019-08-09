e4a70e1435e6bb406d30768198d858a89c0a847f
eric-klimowich <eklimowich@hotmail.com>
2019-08-09 15:58:37 -0400

    Nested callbacks, forecast now relies on data from location

--- app.js
+++ app.js
@@ -2,13 +2,17 @@ const request = require('request')
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
-
-forecast(-75.7088, 44.1545, (error, data) => {
-    console.log('Error', error)
-    console.log('Data', data)
-  })
-
-geocode('Boston', (error, data) => {
-    console.log('Error', error)
-    console.log('Data', data)
+geocode("Boston", (error, locationData) => {
+    if (error) {
+        return console.log(error)
+    }
+    
+    forecast(locationData.longitude, locationData.latitude, (error, forecastData) => {
+        if (error) {
+            console.log(error)
+        }
+        
+        console.log(locationData.location)
+        console.log(forecastData)
+      })
 })
